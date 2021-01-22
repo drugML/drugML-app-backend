0181986874b2281e34c0bfa4ca47b73d48976b54
Benjamin Li <pharmashawarma@gmail.com>
2021-01-21 21:54:06 -0500

    https

---
+++ .platform/confighooks/prebuild/00_save_nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CONFIG_PATH='/etc/nginx/nginx.conf'
+
+# Save nginx configuration to root's home folder
+
+if [ -f "$CONFIG_PATH" ]; then
+    cp "$CONFIG_PATH" ~/
+fi
